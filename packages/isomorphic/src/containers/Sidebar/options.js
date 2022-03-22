674ed966913abe9be30f71376898c358ccbf78e0
mazenalhabeby <a.mazen.dessouky@gmail.com>
2022-03-22 16:06:58 +0200

    commit

--- packages/isomorphic/src/containers/Sidebar/options.js
+++ packages/isomorphic/src/containers/Sidebar/options.js
@@ -53,48 +53,48 @@ const options = [
     leftIcon: 'ion-android-checkbox-outline',
   },
 
-  // {
-  //   key: 'pages',
-  //   label: 'sidebar.pages',
-  //   leftIcon: 'ion-document-text',
-  //   children: [
-  //     {
-  //       key: '404',
-  //       label: 'sidebar.404',
-  //       withoutDashboard: true,
-  //     },
-  //     {
-  //       key: '500',
-  //       label: 'sidebar.500',
-  //       withoutDashboard: true,
-  //     },
-  //     {
-  //       key: 'signin',
-  //       label: 'sidebar.signIn',
-  //       withoutDashboard: true,
-  //     },
-  //     {
-  //       key: 'signup',
-  //       label: 'sidebar.signUp',
-  //       withoutDashboard: true,
-  //     },
-  //     {
-  //       key: 'forgotpassword',
-  //       label: 'sidebar.forgotPw',
-  //       withoutDashboard: true,
-  //     },
-  //     {
-  //       key: 'resetpassword',
-  //       label: 'sidebar.resetPw',
-  //       withoutDashboard: true,
-  //     },
+  {
+    key: 'pages',
+    label: 'sidebar.pages',
+    leftIcon: 'ion-document-text',
+    children: [
+      {
+        key: '404',
+        label: 'sidebar.404',
+        withoutDashboard: true,
+      },
+      {
+        key: '500',
+        label: 'sidebar.500',
+        withoutDashboard: true,
+      },
+      {
+        key: 'signin',
+        label: 'sidebar.signIn',
+        withoutDashboard: true,
+      },
+      {
+        key: 'signup',
+        label: 'sidebar.signUp',
+        withoutDashboard: true,
+      },
+      {
+        key: 'forgotpassword',
+        label: 'sidebar.forgotPw',
+        withoutDashboard: true,
+      },
+      {
+        key: 'resetpassword',
+        label: 'sidebar.resetPw',
+        withoutDashboard: true,
+      },
 
-  //     // {
-  //     //   key: 'comingSoon',
-  //     //   label: 'sidebar.comingSoon',
-  //     //    withoutDashboard: true
-  //     // }
-  //   ],
-  // },
+      // {
+      //   key: 'comingSoon',
+      //   label: 'sidebar.comingSoon',
+      //    withoutDashboard: true
+      // }
+    ],
+  },
 ];
 export default options;
